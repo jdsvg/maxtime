89573bd7dfeec6dafbacfaae4495a0215b87ba28
John Villamil <jdsvg997@outlook.com>
2022-05-23 13:43:19 -0500

    Reorganizando codigo

--- src/main/java/co/com/choucair/certification/maxtime/tasks/GoToTheSectionNewReport.java
+++ src/main/java/co/com/choucair/certification/maxtime/tasks/GoToTheSectionNewReport.java
@@ -5,7 +5,7 @@ import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.Tasks;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.waits.WaitUntil;
-import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isEnabled;
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isCurrentlyEnabled;
 import static co.com.choucair.certification.maxtime.userinterface.GoToTheSectionNewReportPage.*;
 
 public class GoToTheSectionNewReport implements Task {
@@ -17,8 +17,8 @@ public class GoToTheSectionNewReport implements Task {
     @Override
     public <T extends Actor> void performAs(T actor) {
             actor.attemptsTo(
-                    WaitUntil.the(SEARCH_CURRENT_DATE2, isEnabled()).forNoMoreThan(60).seconds()
-                    ,Click.on(SEARCH_CURRENT_DATE2)
+                    WaitUntil.the(SEARCH_CURRENT_DATE, isCurrentlyEnabled()).forNoMoreThan(60).seconds()
+                    ,Click.on(SEARCH_CURRENT_DATE)
             );
     }
 }
--- src/main/java/co/com/choucair/certification/maxtime/userinterface/GoToTheSectionNewReportPage.java
+++ src/main/java/co/com/choucair/certification/maxtime/userinterface/GoToTheSectionNewReportPage.java
@@ -6,10 +6,6 @@ import org.openqa.selenium.By;
 public class GoToTheSectionNewReportPage extends PageObject {
 
     public static final Target SEARCH_CURRENT_DATE = Target.the("pa una puta mierda").located(
-            By.xpath("//table[@id='MT']//table[@id='MRC']//div[@id='CP']//div[@class='Item ListViewItem']//tbody//tbody//tr[2]//td[3]")
-    );
-
-    public static final Target SEARCH_CURRENT_DATE2 = Target.the("pa una puta mierda").located(
             By.cssSelector("div#Content table tbody tr + tr table  tbody td#separatorCell + td table tbody div#CP  table tbody div.Item.ListViewItem table  tbody  table tbody tr +tr td +td +td")
     );
 
